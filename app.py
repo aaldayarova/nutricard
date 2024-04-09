182302191d8c4d530b1e95e152db028b4c6aaa0d
Aika Aldayarova <aika.aldayar@gmail.com>
2024-04-08 20:04:00 -0400

    created dropdown menu

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+# Import necessary libraries
+import pandas as pd
+import streamlit as st
 
-app = Flask(__name__)
+# Save .csv file into a dataset object using pandas library
+df = pd.read_csv("cleaned-nutrition.csv")
 
-@app.route("/")
-def welcome():
-    return "Welcome to the welcome page of NutriCard!"
+# Create a dropdown menu containing our dataset "name" values
+option = st.selectbox(
+    "What grocery item would you like to add?", df["new_name"])
+
+# Print the value chosen by the user
+st.write('You selected:', option)
+
+st.write(df.loc[df["new_name"] == option])
